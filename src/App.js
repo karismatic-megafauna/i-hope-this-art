ec4fc9b78a523b81a820433a750db6b8902ce203
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-10-25 08:28:17 -0700

    Made another art and allow for art switching

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import './App.css';
 
-function sketch (p) {
+function matrix(p) {
   var yCord = 0;
   var rectW = 30;
   var r = 100;
@@ -40,11 +40,85 @@ function sketch (p) {
   };
 };
 
+
+function flower(p) {
+  var centerX = 0;
+  var centerY = 0;
+  var radius = 0;
+  var totalDegrees = 360;
+  var opacity = 80;
+  var r = 200;
+  var g = 200;
+  var b = 255;
+
+  var initialX ;
+  var initialY;
+
+  p.setup = function() {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.background(0);
+    centerX = p.width / 2;
+    centerY = p.height / 2;
+    radius = p.height / 2;
+    p.angleMode(p.DEGREES);
+  }
+
+  p.draw = function() {
+    p.noFill();
+    p.stroke(r, g, b, opacity);
+    p.beginShape();
+    for (var i = 0; i <= totalDegrees; i++) {
+      var noiseFactor = p.noise(i / 45, p.frameCount / 120);
+      var x = centerX + radius * p.cos(i) * noiseFactor;
+      var y = centerY + radius * p.sin(i) * noiseFactor;
+      if(i === 0) {
+        initialX = x;
+        initialY = y;
+      }
+
+      if(i === totalDegrees){
+        var averageX = (initialX + x) / 2;
+        var averageY = (initialY + y) / 2;
+
+        p.curveVertex(averageX, averageY);
+      } else {
+        p.curveVertex(x, y);
+      }
+    }
+    p.endShape(p.CLOSE);
+    radius -= 0.5;
+    opacity -= 0.1;
+    g -= 0.8;
+    r -= 0.3;
+
+    if (radius <= 0) {
+      p.noLoop();
+    }
+  }
+}
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sketches: [ matrix, flower ],
+      active: flower,
+    }
+  }
+
+  switchSketch = () => {
+    const current = this.state.active.name;
+    const nextSketchs = this.state.sketches.filter((sketch) => {
+      return sketch.name !== current;
+    });
+    this.setState({ active: nextSketchs[0]});
+  }
+
   render() {
     return (
-      <div className="App">
-        <P5Wrapper sketch={sketch} />
+      <div className="App" onClick={this.switchSketch}>
+        <P5Wrapper sketch={this.state.active} />
       </div>
     );
   }
